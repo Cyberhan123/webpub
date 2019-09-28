ab1b4c1410e898b0e4fa6a202450746cda30bea3
gonglixin980926 <1480059245@qq.com>
2019-09-28 22:07:11 +0800

    login

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@
 import './css/index.css';
 import App from './view/App.jsx';
 import * as serviceWorker from './serviceWorker';
+import initScreen from "./common/common";
 
+initScreen();
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
