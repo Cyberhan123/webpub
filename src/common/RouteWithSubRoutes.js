be0c29baf8041312880eb2d2ef7d7f9f84349984
Cyberhan123 <255542417@qq.com>
2019-10-01 19:45:54 +0800

    大规模更新pc端首页样式

--- src/common/RouteWithSubRoutes.js
+++ src/common/RouteWithSubRoutes.js
@@ -1,18 +1,18 @@
 // import React from "react";
 // import {Route} from "react-router-dom";
-const { Route } = ReactRouterDOM
+const { Route, Redirect } = ReactRouterDOM
 /**
  * 接受router 对象 对 对象进行映射
  * @param {Object} route 
  */
 export default function RouteWithSubRoutes(route) {
   return (
-    <Route
-      path={route.path}
-      render={props => (
-        // pass the sub-routes down to keep nesting
-        <route.component {...props} routes={route.routes} />
-      )}
-    />
+      <Route
+        path={route.path}
+        render={props => (
+          // pass the sub-routes down to keep nesting
+          <route.component {...props} routes={route.routes} />
+        )}
+      />
   );
 }
